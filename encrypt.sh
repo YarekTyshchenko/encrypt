4fd42304a29109d22c1365c9b5be66df1d617153
Yarek Tyshchenko <yarek@healthforge.io>
2019-02-06 12:53:00 +0000

    Correct indenting

--- encrypt.sh
+++ encrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # encrypt from buffer
 if [[ -z "$1" ]]; then
-		echo "Error: No recepient specified"
-		echo "encrypt.sh foo@bar.com"
-		exit 1
+	echo "Error: No recepient specified"
+	echo "encrypt.sh foo@bar.com"
+	exit 1
 fi
 pbpaste | gpg --encrypt --sign --armor -r $1 - | pbcopy
